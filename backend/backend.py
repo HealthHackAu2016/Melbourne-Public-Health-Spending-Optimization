250753af34c562d0ac60414035000812289c0af9
Ruth Pearson <ruth.pearson8@gmail.com>
2016-10-15 14:31:49 +1100

    getResults function now performs database call and returns best results

--- backend/backend.py
+++ backend/backend.py
@@ -5,18 +5,57 @@ Created on Sat Oct 15 11:04:49 2016
 @author: ruth
 """
 
-def getResult(constraints):
-    # constraints is a dictionary with this structure:
-    # constraints = {}
-    # constraints[intervention] = {}
+import sqlite3
+
+def query(queryText):
+  # Location of database file
+  database_file = "database.db"
+  # Open SQLite database connection
+  conn = sqlite3.connect(database_file)
+  cur = conn.cursor()
+  # do the query
+  result = cur.execute(queryText)
+  return cur.fetchall()
+
+def getResult(interventionConstraints):
+    # interventionConstraints is a dictionary with this structure:
+    # interventionConstraints = {}
+    # interventionConstraints[intervention] = {}
     # example:
-    # constraints[breastfeeding][min] = 10
-    # constraints[breastfeeding][max] = 100
+    # interventionConstraints[breastfeeding][min] = 10
+    # interventionConstraints[breastfeeding][max] = 100
 
-    #run import_data
-
-    # SQL call to get all entries that meet the criteria
-
-    # search through the set returned and return the best
-    
+    # make the SQL query string
+    queryString = 'SELECT * FROM data WHERE'
+    for intervention in interventionConstraints:
+        minStr = str(interventionConstraints[intervention]['min'])   
+        maxStr = str(interventionConstraints[intervention]['max']) 
+        stringToAdd = " " + '"' + intervention + '"' + " BETWEEN '" + minStr + "' AND '" + maxStr + "' AND"
+        queryString += stringToAdd
+   
+   # remove the final 'AND' and put a semicolon on the end
+    queryString = queryString[:len(queryString)-4]   
+    queryString += ';'
+   
+   # SQL call to get all entries that meet the criteria
+    results = query(queryString)
+   
+   # search through the set returned and return the best
+    best = results[0]   
+    for result in results:
+        if result[0] < best[0]:
+            best = result
+   # returb the best         
     return best
+    
+    
+interventionConstraints = {}
+interventionConstraints["Breastfeeding promotion"] = {}
+interventionConstraints["Breastfeeding promotion"]['min'] = 1000
+interventionConstraints["Breastfeeding promotion"]['max'] = 2000
+interventionConstraints["Multiple micronutrient supplementation"] = {}
+interventionConstraints["Multiple micronutrient supplementation"]['min'] = 1000
+interventionConstraints["Multiple micronutrient supplementation"]['max'] = 2000
+    
+best = getResult(interventionConstraints)
+print best    

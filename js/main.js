53f9ce822788fd4627e04849d6b0637dd36c28bf
Sally <sally.pryor@outlook.com>
2016-10-15 20:02:31 +1100

    getting the JavaScript working with Nick's help

--- js/main.js
+++ js/main.js
@@ -1,6 +1,47 @@
 $.fn.serializeObject = function()
 {
 
+var postData = {};
 
-    
-}
+var fdata = this.serializeArray();
+$.each( fdata, function() {
+    if(postData[this.name] !== undefined) {
+        if (!postData[this.name].push) {
+            postData[this.name] = [postData[this.name]];
+        }
+        postData[this.name].push(this.value || '');
+    } else {
+        postData[this.name] = this.value || '';
+    }
+
+});
+return postData;
+
+};
+
+//# curl -i -H "Content-Type: application/json" -X POST -d '{"compfood-min":"1", "compfood-max":"10"}' http://localhost:5000/api/v1.0/query
+
+$(function() {
+    $.ajax()
+    $('#constraints').submit(function(e) {
+        e.preventDefault();
+        resp = {}
+        $('#constraints').serializeArray().forEach(function(form_obj) {
+            resp[form_obj['name']] = form_obj['value'];
+        })
+        console.log(resp);
+
+        var request = $.ajax({
+            url: "http://ryan.cengia.id.au:5000/api/v1.0/query",
+            type: "POST",
+            data: resp,
+            dataType: "application/json"
+        });
+
+        request.done(function(msg) {
+            console.log(msg);
+            // var recommendation = JSON.parse(msg)
+        })
+    });
+
+});
